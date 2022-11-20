07c8eaf3c2ab4ba8811da4cfabf578e7a2929bfc
Kunal soni <97017523+Kunal301@users.noreply.github.com>
2022-11-20 21:46:15 +0530

    js file added

---
+++ script.js
@@ -0,0 +1,47 @@
+
+  
+// For Date
+
+
+  
+var yourCheckbox = document.querySelector('#yourCheckbox');
+var yourDateField = document.querySelector('#yourDateField');
+
+function updateYourDateField() {
+
+	if(yourCheckbox.checked) {
+  	yourDateField.disabled = false;
+  }
+  else {
+  	yourDateField.disabled = true;
+  }
+}
+updateYourDateField();
+
+yourCheckbox.addEventListener('change', function() {
+
+	updateYourDateField();
+})
+
+var Checkbox = document.querySelector('#chkddl');
+var yourTimeField= document.querySelector('#DDL');
+
+function updateYourDDL() {
+
+	if(Checkbox.checked) {
+  	yourTimeField.disabled = false;
+  }
+  else {
+  	yourTimeField.disabled = true;
+  }
+}
+updateYourDDL();
+
+Checkbox.addEventListener('change', function() {
+
+	updateYourDDL();
+})
+
+
+
+
